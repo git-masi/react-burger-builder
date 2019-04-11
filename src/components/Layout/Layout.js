d16f4b973bd336353c9db450a789d8e44b417112
Eric Masi <eric@gitmasi.com>
2019-04-11 10:22:00 -0400

    wip

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 // could also use fragments here
 import Aux from '../../hoc/Auxiliary';
+import styles from './Layout.module.css';
 
 const layout = props => (
   <Aux>
-    <div>toolbar, sidedrawer, backdrop</div>
-    <main>
+    <div className={styles.content}>toolbar, sidedrawer, backdrop</div>
+    <main className={`${styles.content} ${styles.red}`}>
       {props.children}
     </main>
   </Aux>
