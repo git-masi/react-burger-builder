b6895533eebe915dad99339d7b5a604cd202bae9
Eric Masi <eric@gitmasi.com>
2019-04-11 10:27:40 -0400

    wip

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,8 @@ import styles from './Layout.module.css';
 const layout = props => (
   <Aux>
     <div className={styles.content}>toolbar, sidedrawer, backdrop</div>
-    <main className={`${styles.content} ${styles.red}`}>
+    {/* <main className={`${styles.content} ${styles.red}`}> */}
+    <main className={styles.content}>
       {props.children}
     </main>
   </Aux>
