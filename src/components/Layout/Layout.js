8a3f333e3f0f252100227b06b2f15aa1bebb9d2d
Eric Masi <eric@gitmasi.com>
2019-04-11 10:08:49 -0400

    add burgerbuilder component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React from 'react';
 // could also use fragments here
-import Aux from '../../hoc/auxiliary';
+import Aux from '../../hoc/Auxiliary';
 
 const layout = props => (
   <Aux>
     <div>toolbar, sidedrawer, backdrop</div>
     <main>
-      {props.children};
+      {props.children}
     </main>
   </Aux>
 );
