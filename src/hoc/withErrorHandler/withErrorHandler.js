187ae437e90b9675fd780d01e874060976b4ddd9
Eric Masi <eric@gitmasi.com>
2019-07-23 09:08:29 -0400

    add global axios error handling withErrorHandler

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Aux from '../Auxiliary';
+import Modal from '../../components/UI/Modal/Modal';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null
+    }
+
+    componentDidMount () {
+      axios.interceptors.request.use(req => {
+        this.setState({ error: null });
+        return req;
+      })
+      axios.interceptors.response.use(res => res, error => {
+        this.setState({ error: error });
+      })
+    }
+
+    dismissErrorHandler = () => {
+      this.setState({ error: null });
+    }
+
+    render() {
+      return (
+        <Aux>
+          <Modal
+            orderNow={this.state.error}
+            closeModal={this.dismissErrorHandler}>
+            {this.state.error ? this.state.error.message: null}
+            {/* something went wrong! */}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </Aux>
+      )
+    }
+  }
+}
+
+export default withErrorHandler;
